1fcff6f8a8ecbd9432529eda96ac679f5a3087fb
Tiago Sampaiogit config --global user.name Tiago <Teagol10@gmail.com>
2021-07-09 19:49:12 -0300

    Passing a Deparment objeto to DeparmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,9 +9,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable{
 
+	
+	private Department entity;
+	
 	
 	@FXML
 	private TextField txtId;
@@ -28,6 +32,10 @@ public class DepartmentFormController implements Initializable{
 	@FXML
 	private Button btnCancel;
 	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
 	@FXML
 	public void onBtnSaveAction() {
 		System.out.println("onBtnSaveAction");
@@ -38,8 +46,6 @@ public class DepartmentFormController implements Initializable{
 		System.out.println("onBtnCancelAction");
 	}
 	
-	
-	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
@@ -48,6 +54,14 @@ public class DepartmentFormController implements Initializable{
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 
 }
